4bca495f72d4c31375f97c46c4001db192552c1a
LyupoC <lyupogitaccfor@gmail.com>
2024-10-11 22:34:17 +0200

    use secrets for creds

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AuthConfig } from 'angular-oauth2-oidc';
 import { OAuthService, OAuthEvent } from 'angular-oauth2-oidc';
-
+import { environment } from '../../environments/environment';
 
 
 export const authCodeFlowConfig: AuthConfig = {
-  issuer: 'https://dev-iwm1hr4ywzqlodpw.us.auth0.com/',
+  issuer: environment.issuer,
   redirectUri: window.location.origin + '/callback',
-  clientId: 'EEhKnXxSMBYDLCjwlfOl33wRDxSGwecD',
+  clientId: environment.clientId,
   responseType: 'token id_token',
   logoutUrl: window.location.origin,
   scope: 'openid profile email read:profile',
@@ -16,7 +16,7 @@ export const authCodeFlowConfig: AuthConfig = {
   silentRefreshRedirectUri: window.location.origin,
   useSilentRefresh: true,
   customQueryParams: {
-    audience: 'http://localhost:8080',
+    audience: environment.audience ,
     max_age:0
   },
 };
@@ -36,10 +36,9 @@ export class AuthService {
   }
   login(): void {
     this.oauth.initLoginFlow();
-    console.log("access token : " , this.oauth.getAccessToken());
   }
   logout(): void {
-    console.log("logout");
+
     this.oauth.logOut();
     this.oauth.events.subscribe((event) => {
       if (event instanceof OAuthEvent && event.type === 'logout') {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  issuer: process.env['ISSUER'] || 'PROD_ISSUER',
+  clientId: process.env['CLIENT_ID'] || 'PROD_CLIENT_ID',
+  audience: process.env['AUDIENCE'] || 'PROD_AUDIENCE'
 };
